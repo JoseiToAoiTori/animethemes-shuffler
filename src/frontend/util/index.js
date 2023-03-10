e69375f025cb8211f54b853c935928f991591013
JoseiToAoiTori <acciowand200@hotmail.com>
2023-03-10 14:15:09 +0100

    fix edgecases

--- src/frontend/util/index.js
+++ src/frontend/util/index.js
@@ -2,7 +2,10 @@ const superagent = require('superagent');
 
 async function getAnimeThemes (animeString) {
 	const themesResponse = await superagent.get(`https://api.animethemes.moe/anime?filter[has]=resources&filter[site]=AniList&filter[external_id]=${animeString}&include=animethemes.animethemeentries.videos,animethemes.song&page[size]=100&sort=random`);
-	return themesResponse.body.anime.map(anime => anime.animethemes.map(el => ({title: `${anime.name} ${el.slug} - ${el.song.title}`, video: el.animethemeentries[0].videos[0].link}))).flat();
+	return themesResponse.body.anime.map(anime => anime.animethemes.filter(theme => theme.animethemeentries[0].videos.length).map(el => {
+		console.log(el);
+		return {title: `${anime.name} ${el.slug} - ${el.song.title}`, video: el.animethemeentries[0].videos[0].link};
+	})).flat();
 }
 
 export default {
